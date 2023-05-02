a878f8aaf6eb739d9fd4e79d08e82e29574b9b99
Javokhir <javohir.murodov.9238@gmail.com>
2023-05-02 16:35:43 +0500

    regisrer form with validataion has been finished

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  userEmail: string = '';
+  userPassword: string = '';
+  @ViewChild('f') loginForm!:NgForm
 
+  constructor() {}
+
+  onSubmit() {
+    const userData={
+      email:this.userEmail,
+      password:this.userPassword
+    }
+    console.log(userData)
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  @ViewChild('f') registerForm!:NgForm
+  userFullname: string = '';
+  userEmail: string = '';
+  userPassword: string = '';
 
+  constructor() {}
+
+  onSubmit() {
+    const userData={
+      fullname:this.userFullname,
+      email:this.userEmail,
+      password:this.userPassword
+    }
+
+    console.log(userData)
+  }
 }
