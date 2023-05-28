a878ffef7fb52c0edd6c413a741a705e61e55e29
Javokhir <javohir.murodov.9238@gmail.com>
2023-05-28 16:51:05 +0500

    changing API_URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://review-portal-9tfl.onrender.com';
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://review-portal-9tfl.onrender.com';
+// export const API_URL = 'http://localhost:3000';
